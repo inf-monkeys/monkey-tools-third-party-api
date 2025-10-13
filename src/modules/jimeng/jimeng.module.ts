c4cc01440451de6fd04649268caa57cd87d146e9
honker <oncwnuDYMg2ciU29cw30cr3dLOcM@git.weixin.qq.com>
2025-10-13 17:39:28 +0800

    feat:jimeng ark

--- src/modules/jimeng/jimeng.module.ts
+++ src/modules/jimeng/jimeng.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { JimengController } from '@/modules/jimeng/jimeng.controller';
-import { JimengV4Service } from '@/modules/jimeng/jimeng.v4.service';
 import { JimengArkService } from '@/modules/jimeng/jimeng.ark.service';
 
 @Module({
   controllers: [JimengController],
-  providers: [JimengV4Service, JimengArkService],
+  providers: [JimengArkService],
 })
 export class JimengModule {}
